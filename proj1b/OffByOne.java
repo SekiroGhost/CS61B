84884348cbbb5bc239629c5f8fba5f5bf9edb39b
Sekiro <chen7452@umn.edu>
2022-09-28 15:23:00 -0500

    Update OffByOne.java

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,7 +1,7 @@
 public class OffByOne implements CharacterComparator{
     @Override
     public boolean equalChars(char x, char y){
-        int res = x - y;
+        int res = Character.toLowerCase(x) - Character.toLowerCase(y);
 
         if (res == 0){ return true;}
         else {return false;}
