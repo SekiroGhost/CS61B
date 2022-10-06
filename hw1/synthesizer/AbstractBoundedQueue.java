f183b81efa3099c486d525a28b61c5dc037f1a36
Sekiro <chen7452@umn.edu>
2022-10-05 19:12:09 -0500

    Update AbstractBoundedQueue.java

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -4,7 +4,9 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
 
     protected int fillCount;
     protected int capacity;
+    @Override
     public int capacity(){return capacity;}
+    @Override
     public int fillCount(){return fillCount;}
     //public boolean isEmpty();
     //public boolean isFull();
